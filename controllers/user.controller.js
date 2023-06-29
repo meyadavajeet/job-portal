c7142c2ff7977f1a563ce00131eeb5513940148d
meyadavajeet <meyadavajeet@gmail.com>
2023-06-29 15:41:13 +0530

    working on user section

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,20 +1,17 @@
+import { catchAsyncError } from "../middlewares/catch.async.error.js";
 import userModel from "../models/user.model.js";
+import ErrorHandler from "../utils/ErrorHandler.js";
 
-export const getUsers = async (req, res, next) => {
-  try {
-    const users = await userModel.findAllUsers();
-    return res.status(200).send({
-      success: true,
-      message: "",
-      data: users,
-    });
-  } catch (error) {
-    console.log("error in getUser controller", error.message);
-    process.abort();
-  }
-};
+export const getUsers = catchAsyncError(async (req, res, next) => {
+  const users = await userModel.findAllUsers();
+  return res.status(200).send({
+    success: true,
+    message: "",
+    data: users,
+  });
+});
 
-export const updateUsers = async (req, res, next) => {
+export const updateUsers = catchAsyncError(async (req, res, next) => {
   const { firstName, email, location } = req.body;
 
   console.log("inside update user controller");
@@ -30,7 +27,7 @@ export const updateUsers = async (req, res, next) => {
   // }
   const user = await userModel.findOne({ _id: req.params.id });
   if (!user) {
-    next("User with this id  not exist");
+    next(new ErrorHandler("User with this id  not exist", 404));
   }
   if (req.body.firstName) {
     user.firstName = firstName;
@@ -47,7 +44,7 @@ export const updateUsers = async (req, res, next) => {
   await user.save();
   const token = user.createJWT();
 
-  res.status(200).send({
+  return res.status(200).send({
     success: true,
     message: "Successfully Updated!!",
     data: {
@@ -55,4 +52,35 @@ export const updateUsers = async (req, res, next) => {
       token,
     },
   });
-};
+});
+
+export const deleteUsers = catchAsyncError(async (req, res, next) => {
+  const { id } = req.params;
+  const user = await userModel.findById({ _id: id });
+  if (!user) {
+    next(new ErrorHandler("Data not found in our system!!", 404));
+  }
+  await userModel.findByIdAndDelete({ _id: id });
+  return res.status(200).send({
+    success: true,
+    message: "Data deleted.",
+    data: null,
+  });
+});
+
+export const getUserById = catchAsyncError(async (req, res, next) => {
+  const { id } = req.params;
+  const user = await userModel
+    .findById({ _id: id })
+    .select("-createdAt")
+    .select("-updatedAt")
+    .select("-__v");
+  if (!user) {
+    next(new ErrorHandler("Data not found in our system!!", 404));
+  }
+  return res.status(200).send({
+    success: true,
+    message: "",
+    data: user,
+  });
+});
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,4 +10,9 @@ router.get("/get", userAuth, userController.getUsers);
 // update users || PUT
 router.put("/update/:id", userAuth, userController.updateUsers);
 
+// delete users || PUT
+router.delete("/delete/:id", userAuth, userController.deleteUsers);
+
+router.get("/get-by-id/:id", userAuth, userController.getUserById);
+
 export default router;
